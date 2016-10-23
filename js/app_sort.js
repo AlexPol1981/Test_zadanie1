f0883add97dd4ba445245925fd211d37a3f78aea
AlexPol1981 <radiohead80@list.ru>
2016-10-23 19:06:06 +0400

    Don't work table -1

---
+++ js/app_sort.js
@@ -0,0 +1,12 @@
+angular.module('sortApp', [])
+.controller('mainController', function($scope) {
+  $scope.sortType     = 'country'; // значение сортировки по умолчанию
+  $scope.sortReverse  = false;  // обратная сортировка
+  $scope.searchFish   = '';     // значение поиска по умолчанию
+  // список
+  $scope.database = [
+    { country: 'Rusian', city: 'Ulyanovsk', street: 'Lenina',        home: 1, index: 432345, date: '10.04.2014, 12:21' },
+    { country: 'Rusian', city: 'Ulyanovsk', street: 'Narimanova',    home: 2, index: 432453, date: '15.03.2014, 15:34'},
+    { country: 'Rusian', city: 'Moskov',    street: 'Kievskii',      home: 5, index: 432111, date: '01.05.2014, 23:00'},
+    { country: 'Rusian', city: 'Samara',    street: 'Volokalamskay', home: 7, index: 123123, date: '03.02.2015, 14:00'}
+  ];
